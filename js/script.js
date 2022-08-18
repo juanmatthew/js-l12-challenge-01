a6a50c9f0968741c790e2f002665522d85aef56b
juanmatthew <101286477+JuaniMatthew@users.noreply.github.com>
2022-08-17 20:33:45 -0400

    Updated code to randomly select image, display image, and added click event

--- js/script.js
+++ js/script.js
@@ -3,10 +3,34 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function(){
+const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    //console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    //In the value, use randomIndex to grab a single image from your images array
+    const randomImage =  images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    //change innerText of the authorSpan element to the value of the author variable
+    authorSpan.innerText = author;
+    //Set the src attribute of the img variable to the value of the imageAddress variable
+    img.src= imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
+
+    
